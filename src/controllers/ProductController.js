51c4d5a55c9e083a5b89974ddcd7205410841731
Greesha-Bodana <greeshabodana2004@gmail.com>
2026-03-12 18:47:03 +0530

    create product

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,12 +4,57 @@ const getAllProducts = async (req, res) => {
 
     const allProducts = await productSchema.find()
     res.json({
-        message:"all products",
-        data:allProducts
+        message: "all products",
+        data: allProducts
     })
 
+};
+const getProductById = async (req, res) => {
+    //const foundProduct = await productSchema.find({ _id: req.params.id })
+    const foundProduct = await productSchema.findById(req.params.id)
+    if (foundProduct) {
+        res.json({
+            message: "Product found...",
+            data: foundProduct,
+        });
+    } else {
+        res.json({
+            message: "Product not found..."
+        });
+    }
+};
+
+const addProduct = async (req, res) => {
+
+    //console.log("body...", req.body)
+    const savedproduct = await productSchema.create(req.body)
+    res.status(201).json({
+        message: "product saved..",
+        data: savedproduct
+    })
 }
 
+const deleteProduct = async (req, res) => {
+
+    const deletedProduct = await productSchema.findByIdAndDelete(
+        req.params.id,
+    );
+
+    if (deletedProduct) {
+        res.status(200).json({
+            message: "product deleted",
+            data: deletedProduct,
+        });
+    } else {
+        res.status(200).json({
+            message: "product not found to delete"
+        });
+    }
+};
+
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    getProductById,
+    addProduct,
+    deleteProduct
 }
--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -1,4 +1,7 @@
 const router =require("express").Router()
 const ProductController = require("../controllers/ProductController")
 router.get("/products",ProductController.getAllProducts)
+router.get("/product/:id",ProductController.getProductById)
+router.post("/product",ProductController.addProduct)
+router.delete("/product/:id",ProductController.deleteProduct)
 module.exports = router
